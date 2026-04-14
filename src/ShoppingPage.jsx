775f40a9c3333ec530fcadde9e33882bfe4b66d9
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2026-04-14 14:56:27 +0200

    feat: added chopping cards to display items

--- src/ShoppingPage.jsx
+++ src/ShoppingPage.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router";
+import './styles.css'
 
 const Shop = () => {
   return (
-    <>
-      <p>shopping page</p>
-    </>
+    <div className="shop-grid">
+      <div className="shop-card">
+         <img src="#" alt="image"></img>
+         <h3>item title</h3>
+         <h4>item price</h4>
+         <div className="price-bar">
+          <button>-</button>
+          <input type="number" />
+          <button>+</button>
+         </div>
+         <button>add to cart</button>
+      </div>
+    </div>
   );
 };
 
